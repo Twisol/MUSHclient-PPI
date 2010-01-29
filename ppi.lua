274db2116ca158faf54a5b5c61fa2ae87e4f284e
Jonathan Castello <solugon@hotmail.com>
2010-01-28 22:25:19 -0800

    Second revision of PPI-Lua, authored 2010-01-07.

--- ppi.lua
+++ ppi.lua
@@ -1,49 +1,58 @@
-local PPI_list = setmetatable({}, {__mode = "k"})
+local PPI_list = {}
 
--- Takes a table and returns all values at numeric indices.
--- Example:
---  Input: {1, 2, 3, "5", "7", "10"}
---  Returns: 1, 2, 3, "5", "7", "10"
-local function ExplodeParams(tbl)
-  local param = table.remove(tbl, 1)
-  if #tbl > 0 then
-    return param, ExplodeParams(tbl)
-  else
-    return param
+local myID = GetPluginID()
+local myPPI = {}
+
+local params_id  = function(id) return "PPIparams_" .. id  end
+local method_id  = function(id) return "PPImethod_" .. id  end
+local returns_id = function(id) return "PPIreturns_" .. id end
+
+local request_id = function(id) return "PPI_" .. id .. "_REQUEST" end
+local cleanup_id = function(id) return "PPI_" .. id .. "_CLEANUP" end
+
+
+local function request(id, func_name, ...)
+  -- Prepare the arguments
+  local params = {...}
+  
+  ArrayCreate(params_id(id))
+  
+  for i=1,#params do
+    ArraySet(params_id(id), tostring(i), tostring(params[i]))
   end
+
+  SetVariable(params_id(id), ArrayExport(params_id(id), "|"))
+  ArrayDelete(params_id(id))
+  
+  -- Call the method
+  SetVariable(method_id(id), func_name)
+  CallPlugin(id, request_id(id), myID)
+  
+  -- Clean up the arguments
+  DeleteVariable(params_id(id))
+  DeleteVariable(method_id(id))
+  
+  -- Gather the return values
+  local returns = {}
+  
+  ArrayCreate(returns_id(id))
+  ArrayImport(returns_id(id), GetPluginVariable(id, returns_id(myID)), "|")
+  
+  for k,v in pairs(ArrayList(returns_id(id))) do
+    returns[tonumber(k)] = v
+  end
+  ArrayDelete(returns_id(id))
+  
+  -- Have the other plugin clean up its return values
+  CallPlugin(id, cleanup_id(id), myID)
+  
+  return unpack(returns)
 end
 
 -- A 'thunk' is a delayed resolver function.
 local function new_thunk(id, func_name)
   return function(...)
-    local params = {...}
-    
-    -- Prepare the arguments
-    for i=1,#params do
-      SetVariable("param" .. i .. "_" .. id, tostring(params[i]))
-    end
-    
-    -- Call the method
-    SetVariable("method_" .. id, func_name)
-    CallPlugin(id, "PPI_" .. id .. "_PPI", GetPluginID())
-    
-    -- Clean up the arguments
-    for i=1,#params do
-      DeleteVariable("param" .. i .. "_" .. id)
-    end
-    
-    -- Gather the return values
-    local returns = {}
-    local i = 1
-    while GetPluginVariable(id, "return" .. i .. "_" .. GetPluginID()) ~= nil do
-      table.insert(returns, GetPluginVariable(id, "return" .. i .. "_" .. GetPluginID()))
-      i = i + 1
-    end
-    
-    -- Have the other plugin clean up the return values
-    CallPlugin(id, "PPI_" .. id .. "_PPI_CLEAN", GetPluginID())
-    
-    return ExplodeParams(returns)
+    return request(id, func_name, ...)
   end
 end
 
@@ -76,50 +85,41 @@ local PPI = {
   -- Used by a plugin to expose methods to other plugins
   -- through its own PPI.
   Expose = function(name, func)
-    local myPPI = PPI_list[GetPluginID()]
-    myPPI[name] = func
+    myPPI[name] = (func and func or _G[name])
   end,
 }
 
--- create a PPI for this plugin
-myPPI = {}
-PPI_list[myPPI] = {id = GetPluginID()}
-PPI_list[GetPluginID()] = myPPI
-
 -- PPI request resolver
-_G["PPI_" .. GetPluginID() .. "_PPI"] = function(id)
-  local myPPI = PPI_list[GetPluginID()]
-  local myID = PPI_list[myPPI].id
-  if not myPPI then
-    return
-  end
+_G[request_id(myID)] = function(id)
+  ArrayCreate(params_id(id))
+  ArrayImport(params_id(id), GetPluginVariable(id, params_id(myID)), "|")
   
   local params = {}
-  local i = 1
-  while GetPluginVariable(id, "param" .. i .. "_" .. myID) ~= nil do
-    table.insert(params, GetPluginVariable(id, "param" .. i .. "_" .. myID))
-    i = i + 1
+  for k,v in pairs(ArrayList(params_id(id))) do
+    params[tonumber(k)] = v
   end
+  ArrayDelete(params_id(id))
   
-  local func_name = GetPluginVariable(id, "method_" .. myID)
+  local func_name = GetPluginVariable(id, method_id(myID))
   local func = myPPI[func_name]
   if not func then
     return
   end
   
-  local returns = {func(ExplodeParams(params))}
-  for i=1, #returns do
-    SetVariable("return" .. i .. "_" .. id, tostring(returns[i]))
+  local returns = {func(unpack(params))}
+  ArrayCreate(returns_id(id))
+  
+  for i=1,#returns do
+    ArraySet(returns_id(id), tostring(i), tostring(returns[i]))
   end
+
+  SetVariable(returns_id(id), ArrayExport(returns_id(id), "|"))
+  ArrayDelete(returns_id(id))
 end
 
 -- Return value cleaner
-_G["PPI_" .. GetPluginID() .. "_PPI_CLEAN"] = function(id)
-  local i = 1
-  while GetVariable("return" .. i .. "_" .. id) ~= nil do
-    DeleteVariable("return" .. i .. "_" .. id)
-    i = i + 1
-  end
+_G[cleanup_id(myID)] = function(id)
+  DeleteVariable(returns_id(id))
 end
 
 return PPI
